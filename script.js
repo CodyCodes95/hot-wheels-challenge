e1d8163959f7f00dd5ea37117c7212f37029b848
Cody Partington <codythatsme@gmail.com>
2022-05-27 18:54:11 +1000

    Improved terminal readability

--- script.js
+++ script.js
@@ -34,19 +34,20 @@ const setUser = (users) => {
 
 const modSelectorMenu = (mods, budget, userCar) => {
   for (let modType in mods) {
+    console.clear();
     let selector = 1;
     for (let mod of mods[modType]) {
       console.log(`${selector}. ${mod.name}, price: ${mod.price}`);
       mod.selection = selector;
       selector++;
     }
+    console.log(`Current budget: $${budget}`);
     const ans = prompt(`What type of ${modType} would you like? `);
        mods[modType].find(function (el) {
       if (el.selection == ans) {
         if (budget >= el.price) {
           budget = budget - el.price;
           userCar[modType] = el.performance;
-          console.log(userCar);
         } else {
           console.clear();
           console.log("No offence but you're really povvo");
@@ -59,15 +60,16 @@ const modSelectorMenu = (mods, budget, userCar) => {
 };
 
 const race = (userCar, opponentCar) => {
+
+  console.log(`You're matched up against the fierce ${opponentCar.racer[0]}, with a ${opponentCar.motor.name} and ${opponentCar.tyre.name}`);
+  prompt('Press enter to race!')
+  console.clear();
   const userTotal =
     userCar.racerSkill + userCar.motor + userCar.tyre;
   const opponentTotal =
     opponentCar.racer[1] +
     opponentCar.motor.performance +
       opponentCar.tyre.performance;
-
-    console.log(userTotal);
-    console.log(opponentTotal);
 
   if (userTotal > opponentTotal) {
     console.log("You totally Whipped and nae nae'd on that scrub!");
